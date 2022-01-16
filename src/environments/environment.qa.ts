59e8b54919d91667d213d1515db527e3c8123e10
Saurabh Mistry <saurabh.sept18@gmail.com>
2022-01-16 22:47:24 +0530

    updated

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -4,10 +4,10 @@ export const environment = {
 	production: false,
 	current_environment:'local',
 	encryption_key:'Teledaddy_INDIA_Patel_Healthcare',
-	api_url: 'admin-api-qa.teledaddy.com/',
-  client_app_url: 'app-qa.teledaddy.com/',
+	api_url: 'https://admin-api-qa.teledaddy.com/',
+  client_app_url: 'https://app-qa.teledaddy.com/',
 	stripeKey: 'pk_test_J1qdy5OTEEIhXIdoCT1arBrk',
-	websocket_url: 'admin-api-qa.teledaddy.com/',
+	websocket_url: 'https://admin-api-qa.teledaddy.com/',
 	agora_app_id: '51be4b52db464404b35cba61cdbb3a82',
   google_map_api_key:'AIzaSyC6rcSc_T8SsfqPiJBi5W42Oz4jLFXCyLU'
 };
