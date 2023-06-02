e166abedccdab124a024eb17d0b140cd6867b659
Saurabh Mistry <saurabhmistry@Saurabhs-MacBook-Pro.local>
2023-06-02 10:18:44 +0530

    updated

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -4,9 +4,9 @@ export const environment = {
 	production: false,
 	current_environment: 'qa',
 	encryption_key: 'YDF7IirWSIUxR4ijMHXQrpVQ1ZyI0bR6',
-	api_url: 'https://golfed-api.archesoftronic.com/',
+	api_url: 'https://api-qa.golfed-europe.eu/',
 	client_app_url: 'https://golfed-qa.archesoftronix.in/',
 	stripeKey: 'pk_test_51M9tzXE4Jg2znPuBAdh0ElxhTEwhLsk8NVEuaCzCWzR9qZ0LpaZWGXZiKtlsFb9KK6Iqw913Gr0eGLoTIcp0lYyK00k6fyw9kh',
-	websocket_url: 'https://golfed-api.archesoftronic.com/',
+	websocket_url: 'https://api-qa.golfed-europe.eu/',
 	google_map_api_key: ''
 };
